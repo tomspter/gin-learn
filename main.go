c0b65fbfab7a826f87dd41ca1e47342d05980e61
Tomspter <tomspter@163.com>
2024-10-05 20:41:54 +0200

    test webhook

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	// Creates a gin router with default middleware
+	// Creates a gin router with default middleware.
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
